ad776c7e4e00c25055a8fad016cda54c35527e80
Bernd Wachter <bwachter-asvm@lart.info>
2009-05-14 21:28:07 +0200

    added command description

--- svc.c
+++ svc.c
@@ -55,42 +55,54 @@ int main(int argc, char **argv){
   while ((c=getopt(argc, argv, "a:c:d:h:i:k:n:o:p:s:t:u:vx")) != EOF){
     switch(c){
       case 'a':
+        // send SIGALRM to service
         rwcon(cati("A ", optarg, "\n", NULL));
         break;
       case 'c':
+        // send SIGCONT to service
         rwcon(cati("C ", optarg, "\n", NULL));
         break;
       case 'd':
+        // disable service
         rwcon(cati("D ", optarg, "\n", NULL));
         break;
       case 'h':
+        // send SIGHUP to service
         rwcon(cati("H ", optarg, "\n", NULL));
         break;
       case 'i':
+        // send SIGINT to service
         rwcon(cati("I ", optarg, "\n", NULL));
         break;
       case 'k':
+        // send SIGKILL to service
         rwcon(cati("K ", optarg, "\n", NULL));
         break;
       case 'n':
+        // add a new service
         rwcon(cati("N ", optarg, "\n", NULL));
         break;
       case 'o':
         rwcon(cati("O ", optarg, "\n", NULL));
         break;
       case 'p':
+        // send SIGSTOP to service
         rwcon(cati("P ", optarg, "\n", NULL));
         break;
       case 's':
+        // get service status
         rwcon(cati("S ", optarg, "\n", NULL)); 
         break;
       case 't':
+        // send SIGTERM to service
         rwcon(cati("T ", optarg, "\n", NULL));
         break;
       case 'u':
+        // enable service
         rwcon(cati("U ", optarg, "\n", NULL));
         break;
       case 'v':
+        // print version number and usage information
         usage();
         break;
       case 'x':
