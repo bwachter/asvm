56c6378c4754b2a344d54780f2b40ce58c063278
Bernd Wachter <bwachter-oops@lart.info>
2010-01-18 12:40:49 +0100

    Added proper return values on error, svstat-compatibility and usage information

--- svc.c
+++ svc.c
@@ -17,7 +17,13 @@
 #include "config.h"
 #include "version.h"
 
-void rwcon(char *action){
+/*
+ * TODO:
+ * - support pathnames (cut off last part) for better compatibility with
+ *   daemontools
+ */
+
+int rwcon(char *action){
   int infifo, outfifo, i;
   char buf[512];
   char *laction=strdup(action);
@@ -30,13 +36,32 @@ void rwcon(char *action){
   __writefd(infifo, laction);
   if ((i=read(outfifo, buf, 512))>0){
     buf[i]='\0';
-    __write1(buf);
+    if (i>2)
+      __write1(buf+2);
   }
+  if (buf[0]=='+') return 0;
+  if (buf[0]=='-') return 1;
+  return 2;
 }
 
 void usage(){
   __write2(cati("asvm build ", VERSION, "\n\n",
-                "Usage: ",
+                "Usage: \n",
+                "-a     send SIGALRM\n",
+                "-c     send SIGCONT\n",
+                "-d     disable service\n",
+                "-h     send SIGHUP\n",
+                "-i     send SIGINT\n",
+                "-k     send SIGKILL\n",
+                "-n     add a new service\n",
+                "-o     check asvm status\n",
+                "-p     send SIGSTOP\n",
+                "-s     print service status\n",
+                "-t     send SIGTERM\n",
+                "-u     enable service\n",
+                "-v     print version number and usage information\n",
+                "-x     shut down asvm (and kill all services)\n",
+                "\n",
                 NULL));
 }
 
@@ -45,10 +70,13 @@ int main(int argc, char **argv){
 
   if (!strcmp(argv[0]+(strlen(argv[0])-6), "svstat")){
     // loop through argv and print status for each service
+    for (argv++;*argv!=NULL;argv++){
+      rwcon(cati("S ", *argv, "\n", NULL));
+    }
     return 0;
   }
   if (!strcmp(argv[0]+(strlen(argv[0])-4), "svok")){
-    // check service in argv[1] and exit 0 on success, 100 on failure
+    // check if asvm is running and exit 0 on success, 100 on failure
     return 0;
   }
 
@@ -56,58 +84,45 @@ int main(int argc, char **argv){
     switch(c){
       case 'a':
         // send SIGALRM to service
-        rwcon(cati("A ", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("A ", optarg, "\n", NULL));
       case 'c':
         // send SIGCONT to service
-        rwcon(cati("C ", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("C ", optarg, "\n", NULL));
       case 'd':
         // disable service
-        rwcon(cati("D ", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("D ", optarg, "\n", NULL));
       case 'h':
         // send SIGHUP to service
-        rwcon(cati("H ", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("H ", optarg, "\n", NULL));
       case 'i':
         // send SIGINT to service
-        rwcon(cati("I ", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("I ", optarg, "\n", NULL));
       case 'k':
         // send SIGKILL to service
-        rwcon(cati("K ", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("K ", optarg, "\n", NULL));
       case 'n':
         // add a new service
-        rwcon(cati("N ", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("N ", optarg, "\n", NULL));
       case 'o':
-        rwcon(cati("O ", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("O ", optarg, "\n", NULL));
       case 'p':
         // send SIGSTOP to service
-        rwcon(cati("P ", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("P ", optarg, "\n", NULL));
       case 's':
         // get service status
-        rwcon(cati("S ", optarg, "\n", NULL)); 
-        break;
+        return rwcon(cati("S ", optarg, "\n", NULL));
       case 't':
         // send SIGTERM to service
-        rwcon(cati("T ", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("T ", optarg, "\n", NULL));
       case 'u':
         // enable service
-        rwcon(cati("U ", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("U ", optarg, "\n", NULL));
       case 'v':
         // print version number and usage information
         usage();
         break;
       case 'x':
-        rwcon(cati("X", optarg, "\n", NULL));
-        break;
+        return rwcon(cati("X", optarg, "\n", NULL));
     }
   }
   return 0;
